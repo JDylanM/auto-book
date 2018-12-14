8dcdac6b57a077697e25424e020c64feb6811e8a
Dylan Mäenpää <dylanmdeveloper@gmail.com>
2018-12-14 22:36:00 +0100

    Working ver, added test json data

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ import urllib.request, json
 # Int or string?
 
 def get_reservations(url):
+	"""
+	Code for production
+
 	with urllib.request.urlopen(url) as url:
 		data = json.loads(url.read().decode())
+		print("Done...")
+	"""
+
+	with open('test_data.json') as f:
+		data=json.load(f)
 	return data["reservations"]
 
 def init_schedule():
@@ -36,7 +44,7 @@ def insert_reservations(schedule, reservations):
 
 
 #TODO: HERE
-def find_book_times(schedule):
+"""def find_book_times(schedule):
 	book_times = []
 	found = False
 	start_time = 0
@@ -49,20 +57,22 @@ def find_book_times(schedule):
 			if(found):
 
 			found = False:
-
+"""
 
 def main():
 	url = "https://cloud.timeedit.net/liu/web/schema/ri157366X88Z59Q5Z46g7Y35y5056Y03Q09gQY5Q56777.json"
 	reservations = get_reservations(url)
+	print(reservations)
 
 	#Will recieve an array [True, True, True, True, True, True, True, True, True]
 	#                       08-09  09-10 10-11 11-12 13-14 14-15 15-16 16-17
 	# True if free, false if not free
 	schedule = init_schedule()
 	schedule = insert_reservations(schedule, reservations)
-	book_times = find_book_times(schedule)
+	#book_times = find_book_times(schedule)
 
-	#print(schedule)
+	print(schedule)
 
 if __name__ == "__main__":
+	print("hello")
 	main()
