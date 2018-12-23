5034f51e52322aa85297164f32689cb174c0db09
Dylan Mäenpää <dylanmdeveloper@gmail.com>
2018-12-23 16:02:54 +0100

    find_holes now a class

--- find_holes.py
+++ find_holes.py
@@ -1,81 +1,91 @@
 import urllib.request, json
 
-def get_reservations(url):
-	"""
-	Code for production
+class Schedule:
+	def __init__(self, url):
+		self.url = url
+		self.schedule = self.init_schedule()
+		self.reservations = self.get_reservations()
+		#print(self.schedule)
+		#print(self.reservations)
+		self.insert_reservations()
+		print(self.schedule)
 
-	with urllib.request.urlopen(url) as url:
-		data = json.loads(url.read().decode())
-		print("Done...")
-	"""
+	def get_reservations(self):
+		"""
+		Code for production
 
-	with open('test_data.json') as f:
-		data=json.load(f)
-	return data["reservations"]
+		with urllib.request.urlopen(self.url) as url:
+			data = json.loads(url.read().decode())
+			print("Done...")
+		"""
 
-def init_schedule():
-	schedule = []
-	for i in range(9):
-		schedule.append(True)
-	return schedule
+		with open('test_data.json') as f:
+			data=json.load(f)
+		return data["reservations"]
 
-def insert_reservations(schedule, reservations):
-	for reservation in reservations:
-		start_time = int(reservation["starttime"][:2])
+	def init_schedule(self):
+		schedule = []
+		for i in range(9):
+			schedule.append(True)
+		return schedule
 
-		#TODO: Handle after schedule after 17.00
-		if(start_time > 17):
-			continue
+	def insert_reservations(self):
+		for reservation in self.reservations:
+			start_time = int(reservation["starttime"][:2])
 
-		end_time = int(reservation["endtime"][:2])
-		diff = end_time - start_time
-		start_index = start_time - 8
+			#TODO: Handle after schedule after 17.00
+			if(start_time > 17):
+				continue
 
-		#print("array: {}".format(start_index))
-		for i in range(diff):
-			schedule[start_index + i] = False
+			end_time = int(reservation["endtime"][:2])
+			diff = end_time - start_time
+			start_index = start_time - 8
 
-	return schedule
+			#print("array: {}".format(start_index))
+			for i in range(diff):
+				self.schedule[start_index + i] = False
 
 
-def find_holes(schedule):
-	holes = []
-	found = False
-	start_time = 0
-	print("len of schedule {}".format(len(schedule)))
-	for i, free in enumerate(schedule):
-		print(i)
-		if(free):
-			print("free on spot {}".format(i+8))
+	def find_holes(self):
+		holes = []
+		found = False
+		start_time = 0
+		print(self.schedule)
+		print("len of schedule {}".format(len(self.schedule)))
+		for i, free in enumerate(self.schedule):
 			print(i)
-			found = True
-			start_time = i+8
-		else:
-			if(found):
-				end_time = i+8
+			if(free):
+				print("free on spot {}".format(i+8))
+				print(i)
+				found = True
+				start_time = i+8
+			else:
+				if(found):
+					end_time = i+8
+					holes.append([start_time, end_time])
+					found = False
+
+			if(i+1==len(self.schedule) and found):
+				print("true")
+				end_time = i+9
 				holes.append([start_time, end_time])
 				found = False
 
-		if(i+1==len(schedule) and found):
-			print("true")
-			end_time = i+9
-			holes.append([start_time, end_time])
-			found = False
+		return holes
+"""
+	def main():
+		reservations = get_reservations(url)
 
-	return holes
-
-def main():
-	url = "https://cloud.timeedit.net/liu/web/schema/ri157366X88Z59Q5Z46g7Y35y5056Y03Q09gQY5Q56777.json"
-	reservations = get_reservations(url)
-
-	#Will recieve an array [True, True, True, True, True, True, True, True, True]
-	#                       08-09  09-10 10-11 11-12 13-14 14-15 15-16 16-17
-	# True if free, false if not free
-	schedule = init_schedule()
-	schedule = insert_reservations(schedule, reservations)
-	holes = find_holes(schedule)
-	print(schedule)
-	print(holes)
-
+		#Will recieve an array [True, True, True, True, True, True, True, True, True]
+		#                       08-09  09-10 10-11 11-12 13-14 14-15 15-16 16-17
+		# True if free, false if not free
+		schedule = init_schedule()
+		schedule = insert_reservations(schedule, reservations)
+		holes = find_holes(schedule)
+		print(schedule)
+		print(holes)
+"""
 if __name__ == "__main__":
-	main()
+	url = "https://cloud.timeedit.net/liu/web/schema/ri157366X88Z59Q5Z46g7Y35y5056Y03Q09gQY5Q56777.json"
+	schedule = Schedule(url)
+	print(schedule.find_holes())
