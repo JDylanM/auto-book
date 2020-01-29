8c7d52aa61e5e03f59e4e9d7545690b4f0eb85fd
Dylan Mäenpää <JDylanM@github.com>
2020-01-29 17:10:39 +0100

    Functionality added for boilerplate specific calender

--- quickstart.py
+++ quickstart.py
@@ -33,11 +33,12 @@ def main():
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
+    # jhgqt9obpt3i6cogiljuejvq0k@group.calendar.google.com THIS IS kandidat
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
+    events_result = service.events().list(calendarId='58n1hkl3epnfflmlt8iptlqk3blr9vua@import.calendar.google.com', timeMin=now,
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
@@ -48,31 +49,25 @@ def main():
     'description': 'A chance to hear more about Google\'s developer products.',
     'start': {
         'dateTime': '2020-05-28T09:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
+        'timeZone': 'Europe/Stockholm',
     },
     'end': {
         'dateTime': '2020-05-28T17:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    },
-    'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-    ],
-    'attendees': [
-        {'email': 'lpage@example.com'},
-        {'email': 'sbrin@example.com'},
-    ],
-    'reminders': {
-        'useDefault': False,
-        'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': 10},
-        ],
+        'timeZone': 'Europe/Stockholm',
     },
     }
 
-    event = service.events().insert(calendarId='primary', body=event).execute()
+    event = service.events().insert(calendarId='jhgqt9obpt3i6cogiljuejvq0k@group.calendar.google.com', body=event).execute()
     print('Event created: %s' % (event.get('htmlLink')))
-
+    page_token = None
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list['items']:
+            print(calendar_list_entry["id"])
+            print(calendar_list_entry["summary"])
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
 
     if not events:
         print('No upcoming events found.')
